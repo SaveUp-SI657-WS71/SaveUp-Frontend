8894a62dc59477859dce201e276ac689c188527b
Aldo Francisco Pastrana Leon <U20211C186@upc.edu.pe>
2024-06-25 17:36:17 -0500

    Update Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 
     baseURL: "http://localhost:8082/api/saveup/v1",
     baseURLUserService: "http://192.168.56.1:8082/api/saveup/v1",
-    baseURLProductService: "http://192.168.98.136:8083/api/saveup/v1",
+    baseURLProductService: "http://192.168.18.6:8083/api/saveup/v1",
     baseURLCardService: "http://192.168.56.1:8084/api/saveup/v1",
     baseURLOrderService: "http://192.168.56.1:8085/api/saveup/v1"
 };
